a88bae1392b6b32504f6bdd806c3bb7075858c38
Jonatas1678 <teu_lima07@icloud.com>
2025-05-06 18:56:08 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,17 @@
 function toggleMenu() {
-    const menu = document.getElementById('menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
+    const menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
     } else {
-        menu.style.display = 'block';
+        menu.style.display = "block";
     }
 }
+
+document.addEventListener('click', function (event) {
+    const menu = document.getElementById("menu");
+    const button = document.querySelector('.menu-hamburguer');
+
+    if (!menu.contains(event.target) && !button.contains(event.target)) {
+        menu.style.display = "none";
+    }
+});
